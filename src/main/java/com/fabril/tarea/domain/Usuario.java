a0f8bad64b5db4bf491fa054434978e188286551
alfredmadeinperu <alfredmadeinperu@gmail.com>
2023-01-16 17:58:45 -0500

    Nuevos cambios, crear base de datos en mysql con nombre db_usuario.

--- src/main/java/com/fabril/tarea/domain/Usuario.java
+++ src/main/java/com/fabril/tarea/domain/Usuario.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -20,18 +21,18 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "name")
+    @Column(name = "name",length = 30)
     private String Name;
 
-    @Column(name = "lastname")
+    @Column(name = "lastname",length = 30)
     private String lastName;
 
     @Column(name = "dateOfBirth")
-    private String date;
+    private Date date;
 
-    @Column(name = "phone")
+    @Column(name = "phone",length = 25)
     private String phone;
 
-    @Column(name = "address")
+    @Column(name = "address",length = 250)
     private String address;
 }
